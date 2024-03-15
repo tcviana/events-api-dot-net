15b1cff2e94e0ba7e083cdb4f47c94e013aff298
Tiago Viana <tcviana.sp@gmail.com>
2024-03-15 11:56:26 -0300

    Adicionada documentação do swagger.

--- CRUD.API/Controllers/DevEventsController.cs
+++ CRUD.API/Controllers/DevEventsController.cs
@@ -18,6 +18,12 @@ namespace CRUD.API.Controllers
         {
             _content = content;
         }
+
+        /// <summary>
+        /// Busca todos os eventos desde que não estejam deletados
+        /// </summary>
+        /// <returns>Lista de eventos</returns>
+        /// <response code="200">Sucesso</response>
         [HttpGet]
         public IActionResult GetAll()
         {
@@ -25,6 +31,14 @@ namespace CRUD.API.Controllers
 
             return Ok(devEvents);
         }
+
+        /// <summary>
+        /// busca o evento mesmo estando deletado
+        /// </summary>
+        /// <param name="id">Identificador do evento</param>
+        /// <returns>Dados do evento</returns>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="200">Sucesso</response>
         [HttpGet("{id}")]
         public IActionResult GetById(Guid id)
         {
@@ -38,6 +52,16 @@ namespace CRUD.API.Controllers
             }
             return Ok(devEvent);
         }
+
+        /// <summary>
+        /// Cadastrar um evento
+        /// </summary>
+        /// <remarks>
+        /// {"title":"string","description":"string","startDate":"2023-02-27T17:59:14.141Z","endDate":"2023-02-27T17:59:14.141Z"}
+        /// </remarks>
+        /// <param name="devEvent">Dados do evento</param>
+        /// <returns>Evento recém criado</returns>
+        /// <response code="201">Sucesso</response>
         [HttpPost]
         public IActionResult Post(DevEvent devEvent)
         {
@@ -45,6 +69,18 @@ namespace CRUD.API.Controllers
             _content.SaveChanges();
             return CreatedAtAction(nameof(GetById), new { id = devEvent.Id }, devEvent);
         }
+
+        /// <summary>
+        /// Atualiza um evento
+        /// </summary>
+        /// <remarks>
+        /// {"title":"string","description":"string","startDate":"2023-02-27T17:59:14.141Z","endDate":"2023-02-27T17:59:14.141Z"}
+        /// </remarks>
+        /// <param name="id">Identificador do evento</param>
+        /// <param name="input">Dados a serem atualizados</param>
+        /// <returns>Nada</returns>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="204">Sucesso</response>
         [HttpPut("{id}")]
         public IActionResult Put(Guid id, DevEvent input)
         {
@@ -62,6 +98,13 @@ namespace CRUD.API.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Deleta um evento
+        /// </summary>
+        /// <param name="Id">Identificador do evento</param>
+        /// <returns>Nada</returns>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="204">Sucesso</response>
         [HttpDelete]
         public IActionResult Delete(Guid Id)
         {
@@ -79,7 +122,17 @@ namespace CRUD.API.Controllers
             return NoContent();
          }
 
-        // api/dev-events/{id}/speakers
+        /// <summary>
+        /// Cadastrar palestrante
+        /// </summary>
+        /// <remarks>
+        /// {"name":"string","talkTitle":"string","talkDescription":"string","linkedInProfile":"string"}
+        /// </remarks>
+        /// <param name="id">Identificador do evento</param>
+        /// <param name="input">Dados do palestrante</param>
+        /// <returns>Nada</returns>
+        /// <response code="204">Sucesso</response>
+        /// <response code="404">Evento não encontrado</response>
         [HttpPost("{id}/speakers")]
         public IActionResult PostSpeakers(Guid id, DevEventSpeaker speaker)
         {
--- CRUD.API/Program.cs
+++ CRUD.API/Program.cs
@@ -1,5 +1,6 @@
 using CRUD.API.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,24 @@ builder.Services.AddDbContext<DevEventsDbContent>(o => o.UseSqlServer(connection
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CRUD.API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name  = "Tiago Viana",
+            Email = "tiago.viana@mail.com",
+            Url = new Uri("https://github.com/tcviana")
+        }
+    });
+
+    var xmlFile = "CRUD.API.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
